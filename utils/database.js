4d5c74321b914355244b6a5ad5197329ef522eba
gmnode <myemail@gmail.com>
2023-03-20 17:49:53 +0400

    Connected in MySQL

---
+++ utils/database.js
@@ -0,0 +1,13 @@
+const Sequelize = require('sequelize')
+
+const DB_NAME = 'node-todo'
+const USER_NAME = 'root'
+const PASSWORD = '003300gM$'
+
+const sequelize = new Sequelize(DB_NAME, USER_NAME, PASSWORD, {
+    host: 'localhost',
+    dialect: 'mysql'
+})
+
+
+module.exports = sequelize
